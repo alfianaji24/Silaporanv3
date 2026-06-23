ef2b65d75e9ae750b762603512738e6646f5e8df
ALFIAN AJI WAHYUDI <alfianajiwahyudi@gmail.com>
2026-06-23 17:12:13 +0700

    update 230626

--- resources/views/laporan/jadwal_cetak.blade.php
+++ resources/views/laporan/jadwal_cetak.blade.php
@@ -73,7 +73,7 @@
             box-shadow: inset 0 0 0 1px #333;
             border: none;
         }
-        
+
         .datatable3 thead tr:nth-child(2) th {
             top: 40px;
         }
@@ -87,15 +87,21 @@
                 box-shadow: inset 0 0 0 1px #333;
                 border: none !important;
             }
-            
+
             .datatable3 th.sticky-col {
                 z-index: 30;
                 background-color: #024a75;
             }
 
-            .first-col { left: 0; width: 30px; }
-            .second-col { left: 30px; width: 70px; }
-            .third-col { left: 100px; width: 150px; }
+            .first-col {
+                left: 0;
+                width: 30px;
+            }
+
+            .third-col {
+                left: 30px;
+                width: 250px;
+            }
         }
 
         @media print {
@@ -103,10 +109,12 @@
                 overflow: visible;
                 border: none;
             }
+
             .datatable3 {
                 width: 100%;
                 table-layout: auto;
             }
+
             .sticky-col {
                 position: static !important;
             }
@@ -121,9 +129,9 @@
             <tr>
                 <td style="width: 70px; padding-right: 10px;">
                     @if ($generalsetting->logo && Storage::exists('public/logo/' . $generalsetting->logo))
-                        <img src="{{ asset('storage/logo/' . $generalsetting->logo) }}" alt="Logo Perusahaan" style="max-width: 100px;">
+                    <img src="{{ asset('storage/logo/' . $generalsetting->logo) }}" alt="Logo Perusahaan" style="max-width: 100px;">
                     @else
-                        <img src="https://placehold.co/100x100?text=Logo" alt="Logo Default" style="max-width: 100px;">
+                    <img src="https://placehold.co/100x100?text=Logo" alt="Logo Default" style="max-width: 100px;">
                     @endif
                 </td>
                 <td>
@@ -147,101 +155,103 @@
             <thead>
                 <tr>
                     <th rowspan="2" class="sticky-col first-col">No</th>
-                    <th rowspan="2" class="sticky-col second-col">NIK</th>
                     <th rowspan="2" class="sticky-col third-col">Nama Karyawan</th>
                     <th colspan="{{ $jmlhari }}">Tanggal</th>
                 </tr>
                 <tr>
                     @php
-                        $tanggal_loop = $periode_dari;
+                    $tanggal_loop = $periode_dari;
                     @endphp
                     @while (strtotime($tanggal_loop) <= strtotime($periode_sampai))
                         <th style="width: 80px">
-                            {{ date('d', strtotime($tanggal_loop)) }}<br>
-                            {{ getHari(date('Y-m-d', strtotime($tanggal_loop))) }}
+                        {{ date('d', strtotime($tanggal_loop)) }}<br>
+                        {{ getHari(date('Y-m-d', strtotime($tanggal_loop))) }}
                         </th>
                         @php
-                            $tanggal_loop = date('Y-m-d', strtotime('+1 day', strtotime($tanggal_loop)));
+                        $tanggal_loop = date('Y-m-d', strtotime('+1 day', strtotime($tanggal_loop)));
                         @endphp
-                    @endwhile
+                        @endwhile
                 </tr>
             </thead>
             <tbody>
                 @foreach ($karyawan as $d)
-                    @php
-                        $tanggal_loop = $periode_dari;
-                        $mapJadwalByDate = $jadwal_bydate[$d->nik] ?? [];
-                        $mapJadwalGrupByDate = $jadwal_grup_bydate[$d->nik] ?? [];
-                        $mapJadwalByDay = $jadwal_byday[$d->nik] ?? [];
-                    @endphp
-                    <tr>
-                        <td class="sticky-col first-col">{{ $loop->iteration }}</td>
-                        <td class="sticky-col second-col">{{ $d->nik }}</td>
-                        <td class="sticky-col third-col" style="text-align: left">{{ $d->nama_karyawan }}</td>
-                        
-                        @while (strtotime($tanggal_loop) <= strtotime($periode_sampai))
-                            @php
-                                $search = [
-                                    'nik' => $d->nik,
-                                    'tanggal' => $tanggal_loop,
-                                ];
-                                $ceklibur = ceklibur($datalibur, $search);
-                                $nama_hari = getHari($tanggal_loop);
-                                
-                                $jadwal_info = null;
-                                
-                                // 1) By-Date Employee
-                                if (isset($mapJadwalByDate[$tanggal_loop])) {
-                                    $jadwal_info = $mapJadwalByDate[$tanggal_loop];
-                                }
-                                // 2) By-Date Group
-                                elseif (isset($mapJadwalGrupByDate[$tanggal_loop])) {
-                                    $jadwal_info = $mapJadwalGrupByDate[$tanggal_loop];
-                                }
-                                // 3) By-Day Employee
-                                elseif (isset($mapJadwalByDay[$nama_hari])) {
-                                    $jadwal_info = $mapJadwalByDay[$nama_hari];
-                                }
-                                // 4) By-Day Dept/Branch
-                                else {
-                                    $keyDeptCabang = $d->kode_dept . '|' . $d->kode_cabang;
-                                    $mapDept = $jadwal_bydept[$keyDeptCabang] ?? [];
-                                    if (isset($mapDept[$nama_hari])) {
-                                        $jadwal_info = $mapDept[$nama_hari];
-                                    }
-                                    // 5) Global Schedule
-                                    elseif (isset($jadwal_global[$nama_hari])) {
-                                        $jadwal_info = $jadwal_global[$nama_hari];
-                                    }
-                                }
-                                
-                                $bgcolor = '';
-                                $content = '';
-                                
-                                if (!empty($ceklibur)) {
-                                    $bgcolor = 'green';
-                                    $content = '<span style="color:white">LIBUR</span>';
-                                } elseif ($nama_hari == 'Minggu') {
-                                    $bgcolor = 'orange';
-                                }
-                                
-                                if ($jadwal_info) {
-                                    $content = $jadwal_info['nama_jam_kerja'] . '<br><small>' . 
-                                               date('H:i', strtotime($jadwal_info['jam_masuk'])) . '-' . 
-                                               date('H:i', strtotime($jadwal_info['jam_pulang'])) . '</small>';
-                                    if (empty($bgcolor) && !empty($jadwal_info['color'])) {
-                                        $bgcolor = $jadwal_info['color'];
-                                    }
-                                }
-                            @endphp
-                            <td style="background-color: {{ $bgcolor }}; color: {{ !empty($bgcolor) && $bgcolor != 'white' ? 'white' : 'black' }}">
-                                {!! $content !!}
-                            </td>
-                            @php
-                                $tanggal_loop = date('Y-m-d', strtotime('+1 day', strtotime($tanggal_loop)));
-                            @endphp
+                @php
+                $tanggal_loop = $periode_dari;
+                $mapJadwalByDate = $jadwal_bydate[$d->nik] ?? [];
+                $mapJadwalGrupByDate = $jadwal_grup_bydate[$d->nik] ?? [];
+                $mapJadwalByDay = $jadwal_byday[$d->nik] ?? [];
+                @endphp
+                <tr>
+                    <td class="sticky-col first-col">{{ $loop->iteration }}</td>
+                    <td class="sticky-col third-col" style="text-align: left">{{ $d->nama_karyawan }}</td>
+
+                    @while (strtotime($tanggal_loop) <= strtotime($periode_sampai))
+                        @php
+                        $search=[ 'nik'=> $d->nik,
+                        'tanggal' => $tanggal_loop,
+                        ];
+                        $ceklibur = ceklibur($datalibur, $search);
+                        $nama_hari = getHari($tanggal_loop);
+
+                        $jadwal_info = null;
+
+                        // 1) By-Date Employee
+                        if (isset($mapJadwalByDate[$tanggal_loop])) {
+                        $jadwal_info = $mapJadwalByDate[$tanggal_loop];
+                        }
+                        // 2) By-Date Group
+                        elseif (isset($mapJadwalGrupByDate[$tanggal_loop])) {
+                        $jadwal_info = $mapJadwalGrupByDate[$tanggal_loop];
+                        }
+                        // 3) By-Day Employee
+                        elseif (isset($mapJadwalByDay[$nama_hari])) {
+                        $jadwal_info = $mapJadwalByDay[$nama_hari];
+                        }
+                        // 4) By-Day Dept/Branch
+                        else {
+                        $keyDeptCabang = $d->kode_dept . '|' . $d->kode_cabang;
+                        $mapDept = $jadwal_bydept[$keyDeptCabang] ?? [];
+                        if (isset($mapDept[$nama_hari])) {
+                        $jadwal_info = $mapDept[$nama_hari];
+                        }
+                        // 5) Global Schedule
+                        elseif (isset($jadwal_global[$nama_hari])) {
+                        $jadwal_info = $jadwal_global[$nama_hari];
+                        }
+                        }
+
+                        $bgcolor = '';
+                        $content = '';
+
+                        if (!empty($ceklibur)) {
+                        $bgcolor = 'green';
+                        $content = '<span style="color:white">LIBUR</span>';
+                        } elseif ($nama_hari == 'Minggu') {
+                        $bgcolor = 'orange';
+                        }
+
+                        if ($jadwal_info) {
+                        $content = $jadwal_info['nama_jam_kerja'] . '<br><small>' .
+                            date('H:i', strtotime($jadwal_info['jam_masuk'])) . '-' .
+                            date('H:i', strtotime($jadwal_info['jam_pulang'])) . '</small>';
+                        if (empty($bgcolor) && !empty($jadwal_info['color'])) {
+                        $bgcolor = $jadwal_info['color'];
+                        }
+                        } else {
+                        if (empty($bgcolor)) {
+                        $bgcolor = 'red';
+                        $content = '<span style="color:white">LB-K</span>';
+                        }
+                        }
+                        @endphp
+                        <td style="background-color: {{ $bgcolor }}; color: {{ !empty($bgcolor) && $bgcolor != 'white' ? 'white' : 'black' }}">
+                            {!! $content !!}
+                        </td>
+                        @php
+                        $tanggal_loop = date('Y-m-d', strtotime('+1 day', strtotime($tanggal_loop)));
+                        @endphp
                         @endwhile
-                    </tr>
+                </tr>
                 @endforeach
             </tbody>
         </table>
